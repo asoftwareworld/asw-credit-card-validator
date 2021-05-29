632a3efd1b703814cd8e99dd0272358647d0b238
asoftwareworld <asoftwareworld@gmail.com>
2021-05-29 23:15:59 +0530

    added project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'asw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'asw-credit-card-validator';
+}
